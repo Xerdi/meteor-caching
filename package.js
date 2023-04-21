9ecd83f39771364f07792bfb298146cda15c40ec
Erik Nijenhuis <erik@xerdi.com>
2023-04-21 13:43:47 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
     name: 'xerdi:caching',
-    version: '0.0.3',
+    version: '0.0.4',
     summary: 'Universal service worker for caching resources in a Meteor project',
     git: 'https://github.com/Xerdi/meteor-caching.git',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('2.8.1');
+    api.versionsFrom('2.11.0');
     api.use([
         'meteor',
         'ecmascript',
-        'xerdi:logging@0.0.3'
+        'xerdi:logging@0.0.4'
     ]);
     api.addAssets('service-worker.js', 'client');
     api.mainModule('index.js', 'client');
